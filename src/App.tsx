8889aa0b736ef19872a712921241d4b2e617d7f8
Rafael Machado <rafaelmartinsmachado25@gmail.com>
2025-10-17 15:53:37 -0300

    feat: initial project setup with Vite, React, TS, and Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      {/* Nossas seções serão inseridas aqui na Fase 1 */}
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Isso escaneia todos os arquivos relevantes dentro de src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
